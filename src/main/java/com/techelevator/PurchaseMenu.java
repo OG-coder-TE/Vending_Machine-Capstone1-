454c07474f36694b32eec480efb030769f037d05
Spencer Waplinger <spencewap@gmail.com>
2020-10-10 15:47:14 -0500

    Beginning purchase menu items

--- src/main/java/com/techelevator/PurchaseMenu.java
+++ src/main/java/com/techelevator/PurchaseMenu.java
@@ -2,4 +2,36 @@ package com.techelevator;
 
 public class PurchaseMenu {
 
+	
+	//Instance Variables
+	
+	
+	
+	
+	
+	//Constructor
+	
+	
+	
+	
+	//Methods
+	
+	public void purchaseMenuOptions() {
+		
+		System.out.println("Please select from the following options: ");
+		System.
+		
+		
+	}
+	
+	
+	
+	
+	//Getters & Setters
+	
+	
+	//log file
+	Filewriter  
+	
+	
 }
