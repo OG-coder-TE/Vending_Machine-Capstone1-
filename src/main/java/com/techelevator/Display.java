eb67fc0fe5c5896b02f11bbb38cc130235fc0d70
Spencer Waplinger <spencewap@gmail.com>
2020-10-09 19:11:10 -0500

    Changes to display class

---
+++ src/main/java/com/techelevator/Display.java
@@ -0,0 +1,46 @@
+package com.techelevator;
+
+public class Display {
+
+	//Instance Variables
+	
+	
+	
+	
+	//Constructor
+	public Display() {
+		
+		
+	}
+	
+	
+	
+	//Methods
+	public void getMainMenu(){
+		
+		System.out.println("(1) Display Vending Machine Item");
+		System.out.println("(2) Purchase");
+		System.out.println("(3) Exit");
+	}
+	
+	public void getPurchaseMenu() {
+		
+		System.out.println("(1) Feed Money");
+		System.out.println("(2) Select Product");
+		System.out.println("(3) Finish Transaction");
+		
+	}
+	
+	public void displayInventoryWithRemainingQuantity() {
+		
+		
+				
+	}
+	
+	//Getters
+	
+	
+	
+	
+	
+}
