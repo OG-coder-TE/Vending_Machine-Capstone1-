d6e834388cc7a60b32d91a707d06d257a8223963
Spencer Waplinger <spencewap@gmail.com>
2020-10-10 23:15:03 -0500

    Deleted some stuff

--- src/main/java/com/techelevator/MainMenu.java
+++ src/main/java/com/techelevator/MainMenu.java
@@ -9,57 +9,10 @@ public class MainMenu {
 	
 	
 	
-	
-	}
-<<<<<<< HEAD
-=======
 
-
-
-
-	//Methods
-	public void getMainMenu(){
-			
-		System.out.println("(1) Display Vending Machine Item");
-		System.out.println("(2) Purchase");
-		System.out.println("(3) Exit");
-		
-		System.out.println("Please, choose what you would like to do!");
-		userChoice = inputUser.nextLine();
-		
-		if(userChoice.equals("1")) {
-			Map<String, Product> inventoryObject = Inventory.getInventoryMap();
-			System.out.println(inventoryObject);
-			System.out.println();
-			getMainMenu();
-		}
-			
-		if(userChoice.contentEquals("2")) {
-				
-				System.out.println();
-				System.out.println("Great! What's next?");
-				purchaseMenuObject.displayMenu();
-		
-			}
-		
-		if(userChoice.contentEquals("3")) {
-			System.out.println("Goodbye!");
-			System.exit(1);
-		}
-	}
-	
 	}
 
 	
 		
-		
-<<<<<<< HEAD
-	}
-=======
-	
-				
-	
-	
->>>>>>> 1e75147a3308c85518460d01dc5d8fdf88a333f5
->>>>>>> 810abaeb121dd156b8f908e32499ebd1cacb46af
+
 	
